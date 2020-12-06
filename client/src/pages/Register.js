2ec0a19be3beeecd233b53005fb3a6090c74d9ea
abdullahyildiz111 <abdullahyildiz111@gmail.com>
2020-12-07 00:18:32 +0300

    Çok değişik şeyler oluyor

---
+++ client/src/pages/Register.js
@@ -0,0 +1,75 @@
+//import { values } from 'lodash';
+import React, {useState} from 'react';
+import { Form, Button } from 'semantic-ui-react';
+
+function Register() {
+
+    const[values,setValues] = useState({
+        username:'',
+        email:'',
+        password:'',
+        confirmPassword:'',
+    })
+
+    const onChange = (event)=>{
+        setValues({...values, [event.target.name]: event.target.value})
+    }
+
+    const onSubmit = (event)=>{
+        event.preventDefault();
+    }
+
+    return (
+        <div>
+            <Form onSubmit={onSubmit} noValidate>
+                <h1>Register</h1>
+                <Form.Input
+                    label="Username"
+                    placeholder="Username.."
+                    name="username"
+                    value={values.username}
+                    onChange={onChange} />
+
+                <Form.Input
+                    label="Email"
+                    placeholder="Email..."
+                    name="email"
+                    value={values.email}
+                    onChange={onChange} />
+
+                <Form.Input
+                    label="Password"
+                    placeholder="Password.."
+                    name="password"
+                    value={values.password}
+                    onChange={onChange} />
+
+                <Form.Input
+                    label=" Confirm Password"
+                    placeholder=" Confirm Password.."
+                    name="confirmPassword"
+                    value={values.confirmPassword}
+                    onChange={onChange} />
+
+                <Button type="submit" primary>
+                    Register
+                </Button>
+            </Form>
+        </div>
+    )
+}
+
+const REGİSTER_USER = gql`
+mutation register(
+    $username:String!
+    $password:String!
+    $email:String!
+    $confirmPassword:String!
+){
+    register(
+        registerInput:{}
+    )
+}
+`
+
+export default Register;
