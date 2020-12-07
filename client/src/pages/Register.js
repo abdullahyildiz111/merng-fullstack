24f7933ac522d54414dccb8ee392005fa25fc6de
abdullahyildiz111 <abdullahyildiz111@gmail.com>
2020-12-08 00:22:22 +0300

    login and register pages

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,60 +1,87 @@
 //import { values } from 'lodash';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Form, Button } from 'semantic-ui-react';
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
+import {useForm} from '../util/hooks';
 
-function Register() {
-
-    const[values,setValues] = useState({
-        username:'',
-        email:'',
-        password:'',
-        confirmPassword:'',
+function Register(props) {
+    const [errors, setErrors] = useState({});
+    
+    const {onChange, onSubmit, values} = useForm(registerUser,{
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
     })
 
-    const onChange = (event)=>{
-        setValues({...values, [event.target.name]: event.target.value})
-    }
+    const [addUser, { loading }] = useMutation(REGİSTER_USER,{
+               update(_, result) {
+                props.history.push('/');
+            },
+            onError(err) {
+                setErrors(err.graphQLErrors[0].extensions.exception.errors)
+            },
 
-    const onSubmit = (event)=>{
-        event.preventDefault();
-    }
+            variables: values
+        });
+
+        function registerUser(){
+            addUser();
+        }
 
     return (
-        <div>
-            <Form onSubmit={onSubmit} noValidate>
+        <div className="form-container">
+            <Form onSubmit={onSubmit} noValidate className={loading ? 'loading' : ''} >
                 <h1>Register</h1>
                 <Form.Input
                     label="Username"
                     placeholder="Username.."
                     name="username"
+                    type='text'
                     value={values.username}
+                    error={errors.username ? true : false}
                     onChange={onChange} />
 
                 <Form.Input
                     label="Email"
                     placeholder="Email..."
                     name="email"
+                    type='email'
                     value={values.email}
+                    error={errors.email ? true : false}
                     onChange={onChange} />
 
                 <Form.Input
                     label="Password"
                     placeholder="Password.."
                     name="password"
+                    type='password'
                     value={values.password}
+                    error={errors.password ? true : false}
                     onChange={onChange} />
 
                 <Form.Input
                     label=" Confirm Password"
-                    placeholder=" Confirm Password.."
+                    placeholder=" Confiiirm Password.."
                     name="confirmPassword"
+                    type='password'
                     value={values.confirmPassword}
+                    error={errors.confirmPassword ? true : false}
                     onChange={onChange} />
 
                 <Button type="submit" primary>
                     Register
                 </Button>
-            </Form>
+                 </Form>
+                {Object.keys(errors).length > 0 && (<div className='ui error message'>
+                    <ul className='list'>
+                        {Object.values(errors).map(value => (
+                            <li key={value}>{value}</li>
+                        ))}
+                    </ul>
+                </div>)}
+           
         </div>
     )
 }
@@ -62,13 +89,20 @@ function Register() {
 const REGİSTER_USER = gql`
 mutation register(
     $username:String!
+    $email:String!    
     $password:String!
-    $email:String!
     $confirmPassword:String!
 ){
     register(
-        registerInput:{}
-    )
+        registerInput:{
+            username:$username
+            email:$email
+            password:$password
+            confirmPassword:$confirmPassword
+        }
+    ){
+        id email username createdAt token
+    }
 }
 `
 
